c38677c8771721a3f1b14346439cc3c18be6505b
Carl Tewksbury <tewks@Carls-MacBook-Pro.local>
2018-07-16 09:54:30 -0700

    const pointers

--- c/src/pointers.c
+++ c/src/pointers.c
@@ -3,15 +3,33 @@
 int main()
 {
     int foo = 10;
-    char bar[10] = "Foo!";
     int *foo_p = &foo;
 
     printf("address of foo: %p\n", &foo);
-    printf("address of bar: %p\n", &bar);
     printf("value of foo_p: %p\n", foo_p);
     printf("address of foo_p: %p\n", &foo_p);
     printf("dereference foo_p: %d\n", *foo_p);
     printf("sizeof foo_p: %lu\n", sizeof(foo_p));
+
+    int bar = 42;
+    const int *bar_p = &bar;
+
+    printf("const on pointer indirect value (deref): const int *bar_p = &bar;\n");
+    printf("bar_p -> bar: %d\n", bar);
+    bar_p = &foo;
+    printf("update bar_p -> foo: %d\n", foo);
+    foo = 69;
+    printf("update foo directly: %d\n", foo);
+    printf("attempt to update *bar_p = 42; results in:\n");
+    printf("pointers.c:23:12: error: read-only variable is not assignable\n");
+
+    int *const foobar_p = &foo;
+    printf("const on pointer value (addy of var): int *const foobar_p = "
+            "&foo : %d\n", *foobar_p); 
+    printf("attempt to update foobar_p = &bar; results in:\n");
+    printf("pointers.c:28:14: error: read-only variable is not assignable\n");
+    printf("\n");
+    printf("also possible to combine both: const int *const foobar_p = &foo;\n");
 
     return 0;
 
